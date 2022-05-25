5e0150695331346b65b2a3b6005e39adcb255906
Okay Kaçar <44809357+okaykacar@users.noreply.github.com>
2022-05-25 03:55:37 +0300

    AIP 2 | 1
    AIP 2 | 1

---
+++ SchoolRegister.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SchoolRegister.Web.Areas.Identity.Pages.Account {
+   using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace SchoolRegister.Web.Areas.Identity.Pages.Account {
+public class RegisterModel : PageModel {
+public IActionResult OnGet () {
+return NotFound ();
+}
+public IActionResult OnPost () {
+return NotFound ();
+}
+}
+}
+
+}
